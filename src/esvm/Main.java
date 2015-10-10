01854f9d24822cb7f0bcffbe144c6e5aa14448e1
Serbis <OVVYALKIN@gmail.com>
2015-10-10 21:25:25 +0300

    First

--- src/esvm/Main.java
+++ src/esvm/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         App app = new App();
-
+        //Bla bla
         FXMLAMain fxmlaMain;
         FXMLLoader loader = new FXMLLoader(getClass().getResource("controllers/main.fxml"));
         VBox pane = null;
